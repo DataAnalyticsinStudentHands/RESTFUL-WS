0d8d055acae05a8bc8e1c56fa84d23ffa1990b44
Peggy Lindner <peggylind@gmail.com>
2014-07-09 08:40:37 -0500

    users table creation sql

---
+++ mysql/create_users_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(30) NOT NULL,
+  `lastName` varchar(30) NOT NULL,
+  `city` varchar(20) DEFAULT NULL,
+  `homePhone` varchar(10) DEFAULT NULL,
+  `cellPhone` varchar(10) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `picture` varchar(200) DEFAULT NULL,
+  `insertation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
