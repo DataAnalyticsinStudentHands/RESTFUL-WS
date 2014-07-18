a9a4ede6727393af5b1c638d7fe26b93b115c8b1
Peggy Lindner <peggylind@gmail.com>
2014-07-18 09:30:23 -0500

    removed authorities table sql

--- mysql/create_authorities_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `authorities` (
-  `username` varchar(50) NOT NULL,
-  `authority` varchar(50) NOT NULL,
-  UNIQUE KEY `ix_auth_username` (`username`,`authority`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
