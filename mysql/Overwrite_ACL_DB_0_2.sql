05aa93fcd3a955929631c9d4e51bbd3fba23e3ce
Peggy Lindner <peggylind@gmail.com>
2014-07-25 15:45:31 -0500

    started merge with latest VolunteerApp branch

--- mysql/Overwrite_ACL_DB_0_2.sql
+++ mysql/Overwrite_ACL_DB_0_2.sql
@@ -3,10 +3,11 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 18, 2014 at 07:47 PM
+-- Generation Time: Jul 24, 2014 at 08:27 PM
 -- Server version: 5.6.16-log
 -- PHP Version: 5.5.9
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -26,18 +27,25 @@ SET time_zone = "+00:00";
 -- Table structure for table `acl_class`
 --
 
+DROP TABLE IF EXISTS `acl_class`;
 CREATE TABLE IF NOT EXISTS `acl_class` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `class` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_uk_2` (`class`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
+--
+-- Truncate table before insert `acl_class`
+--
+
+TRUNCATE TABLE `acl_class`;
 --
 -- Dumping data for table `acl_class`
 --
 
 INSERT INTO `acl_class` (`id`, `class`) VALUES
+(7, 'dash.pojo.Group'),
 (4, 'dash.pojo.User');
 
 -- --------------------------------------------------------
@@ -46,6 +54,7 @@ INSERT INTO `acl_class` (`id`, `class`) VALUES
 -- Table structure for table `acl_entry`
 --
 
+DROP TABLE IF EXISTS `acl_entry`;
 CREATE TABLE IF NOT EXISTS `acl_entry` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `acl_object_identity` bigint(20) NOT NULL,
@@ -58,8 +67,13 @@ CREATE TABLE IF NOT EXISTS `acl_entry` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_uk_4` (`acl_object_identity`,`ace_order`),
   KEY `foreign_fk_5` (`sid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=108 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=137 ;
 
+--
+-- Truncate table before insert `acl_entry`
+--
+
+TRUNCATE TABLE `acl_entry`;
 --
 -- Dumping data for table `acl_entry`
 --
@@ -67,10 +81,13 @@ CREATE TABLE IF NOT EXISTS `acl_entry` (
 INSERT INTO `acl_entry` (`id`, `acl_object_identity`, `ace_order`, `sid`, `mask`, `granting`, `audit_success`, `audit_failure`) VALUES
 (1, 10, 0, 4, 1, 1, 1, 1),
 (2, 10, 1, 4, 2, 1, 0, 0),
-(99, 45, 0, 34, 1, 1, 0, 0),
-(105, 46, 0, 35, 1, 1, 0, 0),
-(106, 46, 1, 35, 2, 1, 0, 0),
-(107, 46, 2, 35, 8, 1, 0, 0);
+(127, 46, 0, 35, 1, 1, 0, 0),
+(128, 46, 1, 35, 2, 1, 0, 0),
+(129, 46, 2, 35, 8, 1, 0, 0),
+(130, 45, 0, 34, 1, 1, 0, 0),
+(134, 57, 0, 39, 1, 1, 0, 0),
+(135, 57, 1, 39, 2, 1, 0, 0),
+(136, 57, 2, 39, 8, 1, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -78,6 +95,7 @@ INSERT INTO `acl_entry` (`id`, `acl_object_identity`, `ace_order`, `sid`, `mask`
 -- Table structure for table `acl_object_identity`
 --
 
+DROP TABLE IF EXISTS `acl_object_identity`;
 CREATE TABLE IF NOT EXISTS `acl_object_identity` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `object_id_class` bigint(20) NOT NULL,
@@ -89,16 +107,23 @@ CREATE TABLE IF NOT EXISTS `acl_object_identity` (
   UNIQUE KEY `unique_uk_3` (`object_id_class`,`object_id_identity`),
   KEY `foreign_fk_1` (`parent_object`),
   KEY `foreign_fk_3` (`owner_sid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=58 ;
 
+--
+-- Truncate table before insert `acl_object_identity`
+--
+
+TRUNCATE TABLE `acl_object_identity`;
 --
 -- Dumping data for table `acl_object_identity`
 --
 
 INSERT INTO `acl_object_identity` (`id`, `object_id_class`, `object_id_identity`, `parent_object`, `owner_sid`, `entries_inheriting`) VALUES
 (10, 4, 1, NULL, 4, 0),
-(45, 4, 7, NULL, 34, 1),
-(46, 4, 8, NULL, 35, 1);
+(45, 4, 7, NULL, 4, 1),
+(46, 4, 8, NULL, 35, 1),
+(56, 7, 15, NULL, 35, 1),
+(57, 4, 12, NULL, 39, 1);
 
 -- --------------------------------------------------------
 
@@ -106,20 +131,27 @@ INSERT INTO `acl_object_identity` (`id`, `object_id_class`, `object_id_identity`
 -- Table structure for table `acl_sid`
 --
 
+DROP TABLE IF EXISTS `acl_sid`;
 CREATE TABLE IF NOT EXISTS `acl_sid` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `principal` tinyint(1) NOT NULL,
   `sid` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_uk_1` (`sid`,`principal`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=36 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40 ;
 
+--
+-- Truncate table before insert `acl_sid`
+--
+
+TRUNCATE TABLE `acl_sid`;
 --
 -- Dumping data for table `acl_sid`
 --
 
 INSERT INTO `acl_sid` (`id`, `principal`, `sid`) VALUES
-(4, 1, 'tyler'),
+(39, 1, 'Admin'),
+(4, 1, 'Root'),
 (35, 1, 'User'),
 (34, 1, 'Visitor');
 
@@ -129,6 +161,7 @@ INSERT INTO `acl_sid` (`id`, `principal`, `sid`) VALUES
 -- Table structure for table `authorities`
 --
 
+DROP TABLE IF EXISTS `authorities`;
 CREATE TABLE IF NOT EXISTS `authorities` (
   `username` varchar(50) NOT NULL,
   `authority` varchar(50) NOT NULL,
@@ -136,13 +169,47 @@ CREATE TABLE IF NOT EXISTS `authorities` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Truncate table before insert `authorities`
+--
+
+TRUNCATE TABLE `authorities`;
+--
 -- Dumping data for table `authorities`
 --
 
 INSERT INTO `authorities` (`username`, `authority`) VALUES
-('tyler', 'ROLE_ADMIN'),
+('Admin', 'ROLE_ADMIN'),
+('Root', 'ROLE_ROOT'),
 ('User', 'ROLE_USER'),
 ('Visitor', 'ROLE_VISITOR');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_data`
+--
+
+DROP TABLE IF EXISTS `group_data`;
+CREATE TABLE IF NOT EXISTS `group_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `creation_timestamp` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+
+--
+-- Truncate table before insert `group_data`
+--
+
+TRUNCATE TABLE `group_data`;
+--
+-- Dumping data for table `group_data`
+--
+
+INSERT INTO `group_data` (`id`, `name`, `description`, `creation_timestamp`) VALUES
+(15, 'TestGroup', 'This is the description, ''user'' is the group manager', '2014-07-24 10:29:42');
 
 -- --------------------------------------------------------
 
@@ -150,6 +217,7 @@ INSERT INTO `authorities` (`username`, `authority`) VALUES
 -- Table structure for table `login`
 --
 
+DROP TABLE IF EXISTS `login`;
 CREATE TABLE IF NOT EXISTS `login` (
   `username` varchar(50) NOT NULL,
   `password` varchar(128) NOT NULL,
@@ -160,13 +228,19 @@ CREATE TABLE IF NOT EXISTS `login` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
 
 --
+-- Truncate table before insert `login`
+--
+
+TRUNCATE TABLE `login`;
+--
 -- Dumping data for table `login`
 --
 
 INSERT INTO `login` (`username`, `password`, `enabled`, `id`) VALUES
-('tyler', 'test', 1, 1),
+('Root', 'test', 1, 1),
 ('Visitor', 'test', 1, 7),
-('User', 'test', 1, 8);
+('User', 'test', 1, 8),
+('Admin', 'test', 1, 12);
 
 -- --------------------------------------------------------
 
@@ -174,6 +248,7 @@ INSERT INTO `login` (`username`, `password`, `enabled`, `id`) VALUES
 -- Table structure for table `user_data`
 --
 
+DROP TABLE IF EXISTS `user_data`;
 CREATE TABLE IF NOT EXISTS `user_data` (
   `username` varchar(50) NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -187,16 +262,22 @@ CREATE TABLE IF NOT EXISTS `user_data` (
   `insertion_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
+--
+-- Truncate table before insert `user_data`
+--
+
+TRUNCATE TABLE `user_data`;
 --
 -- Dumping data for table `user_data`
 --
 
 INSERT INTO `user_data` (`username`, `id`, `firstName`, `lastName`, `city`, `homePhone`, `cellPhone`, `email`, `picture`, `insertion_date`) VALUES
-('tyler', 1, 'bobby', 'joe', NULL, NULL, NULL, NULL, NULL, '2014-07-09 16:41:51'),
+('Root', 1, '', '', NULL, NULL, NULL, NULL, NULL, '2014-07-09 16:41:51'),
 ('Visitor', 7, 'Client', 'Device', NULL, NULL, NULL, NULL, NULL, '2014-07-18 12:12:54'),
-('User', 8, 'Demo', 'ofUser', NULL, NULL, NULL, NULL, NULL, '2014-07-18 12:14:26');
+('User', 8, 'Demo', 'ofUser', NULL, NULL, NULL, NULL, NULL, '2014-07-18 12:14:26'),
+('Admin', 12, 'Demo', 'ofAdmin', NULL, NULL, NULL, NULL, NULL, '2014-07-24 10:38:34');
 
 --
 -- Constraints for dumped tables
@@ -221,20 +302,21 @@ ALTER TABLE `acl_object_identity`
 -- Constraints for table `acl_sid`
 --
 ALTER TABLE `acl_sid`
-  ADD CONSTRAINT `acl_sid_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `user_data` (`username`) ON DELETE CASCADE;
+  ADD CONSTRAINT `acl_sid_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `user_data` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `authorities`
 --
 ALTER TABLE `authorities`
-  ADD CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `login` (`username`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `login` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `login`
 --
 ALTER TABLE `login`
-  ADD CONSTRAINT `login->user_data (id)` FOREIGN KEY (`id`) REFERENCES `user_data` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `login->user_data (username)` FOREIGN KEY (`username`) REFERENCES `user_data` (`username`) ON DELETE CASCADE;
+  ADD CONSTRAINT `login->user_data (username)` FOREIGN KEY (`username`) REFERENCES `user_data` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `login->user_data (id)` FOREIGN KEY (`id`) REFERENCES `user_data` (`id`) ON DELETE CASCADE;
+SET FOREIGN_KEY_CHECKS=1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
