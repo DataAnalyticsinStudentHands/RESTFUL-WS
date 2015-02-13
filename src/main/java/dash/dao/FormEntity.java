8967804eb5a752f4d089b7c3061fe41d3953d2bd
Chris Holley <cmholley97@gmail.com>
2015-02-13 14:56:44 -0600

    New fields added to forms POJO and Entity

--- src/main/java/dash/dao/FormEntity.java
+++ src/main/java/dash/dao/FormEntity.java
@@ -12,6 +12,8 @@ import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -23,6 +25,7 @@ import org.apache.commons.beanutils.BeanUtils;
 
 import dash.pojo.Entry;
 import dash.pojo.Form;
+import dash.pojo.Form.THEME;
 import dash.pojo.Question;
 
 /**
@@ -36,7 +39,7 @@ import dash.pojo.Question;
 public class FormEntity implements Serializable {
 
 	private static final long serialVersionUID = -8039686696076337053L;
-
+	
 	/** id of the user
 	 * Be aware that every object/entity MUST have an id */
 	@Id
@@ -56,14 +59,66 @@ public class FormEntity implements Serializable {
 	@CollectionTable(name = "questions", joinColumns = {@JoinColumn(name="form_id")})
 	private Set<Question> questions= new HashSet<Question>();
 
+	@Column(name = "public")
+	private boolean publi;
+	
+	@Column(name = "enabled")
+	private boolean enabled;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "theme")
+	private THEME theme;
+	
+	@Column(name = "redirect_to_url")
+	private boolean redirect_to_url;
+	
+	@Column(name = "alert_for_response")
+	private boolean alert_for_response;
+
+	@Column(name = "email_embedded_responses")
+	private boolean email_embedded_responses;
+	
+	@Column(name = "send_confirmation_email")
+	private boolean send_confirmation_email; 
+	
+	@Column(name = "email_message")
+	private String email_message;
+	
+	@Column(name = "completed_message")
+	private String completed_message;
+	
+	@Column(name = "redirect_url")
+	private String redirect_url;
+	
+	@Column(name = "expiration_date")
+	private Date expiration_date;
+	
+	@Column(name = "closed_message")
+	private String closed_message;
+	
 	public FormEntity(){}
 
-	public FormEntity(String name, Set<Question> questions) {
+	public FormEntity(String name, Set<Question> questions, boolean redirect_to_url,
+			boolean enabled, boolean publi, boolean alert_for_response,
+			boolean email_embedded_responses, boolean send_confirmation_email, 
+			String email_message, String completed_message, String redirect_url, 
+			Date expiration_date, String closed_message, THEME theme) {
 		super();
 		this.name = name;
 		this.questions = questions;
+		this.redirect_to_url = redirect_to_url;
+		this.enabled = enabled;
+		this.publi = publi;
+		this.alert_for_response = alert_for_response;
+		this.email_embedded_responses = email_embedded_responses;
+		this.send_confirmation_email = send_confirmation_email;
+		this.email_message = email_message;
+		this.completed_message = completed_message;
+		this.redirect_url = redirect_url;
+		this.expiration_date = expiration_date;
+		this.closed_message = closed_message;
+		this.theme = theme;
 	}
-
 
 	public FormEntity(Form form) {
 		try {
@@ -121,7 +176,98 @@ public class FormEntity implements Serializable {
 		this.questions = questions;
 	}
 
+	public boolean getPubli(){
+		return publi;
+	}
 	
+	public void setPubli(boolean newPubli){
+		this.publi = newPubli;
+	}
 	
-
+	public boolean getEnabled(){
+		return enabled;
+	}
+	
+	public void setEnabled(boolean newEnabled){
+		this.enabled = newEnabled;
+	}
+	
+	public THEME getTheme(){
+		return theme;
+	}
+	
+	public void setTheme(THEME newTheme){
+		this.theme = newTheme;
+	}
+	public boolean getRedirect_to_url(){
+		return redirect_to_url;
+	}
+	
+	public void setRedirect_to_url(boolean newRedirect_to_url){
+		this.redirect_to_url = newRedirect_to_url;
+	}
+	
+	public boolean getAlert_for_response(){
+		return alert_for_response;
+	}
+	
+	public void setAlert_for_response(boolean newAlert_for_response){
+		this.alert_for_response = newAlert_for_response;
+	}
+	
+	public boolean getEmail_embedded_responses(){
+		return email_embedded_responses;
+	}
+	
+	public void setEmail_embedded_responses(boolean newEmail_embedded_responses){
+		this.email_embedded_responses = newEmail_embedded_responses;
+	}
+	
+	public boolean getSend_confirmation_email(){
+		return send_confirmation_email;
+	}
+	
+	public void setSend_confirmation_email(boolean newSend_confirmation_email){
+		this.send_confirmation_email = newSend_confirmation_email;
+	}
+	
+	public String getEmail_message(){
+		return email_message;
+	}
+	
+	public void setEmail_message(String newEmail_message){
+		this.email_message = newEmail_message;
+	}
+	
+	public String getCompleted_message(){
+		return completed_message;
+	}
+	
+	public void setCompleted_message(String newCompleted_message){
+		this.completed_message = newCompleted_message;
+	}
+	
+	public String getRedirect_url(){
+		return redirect_url;
+	}
+	
+	public void setRedirect_url(String newRedirect_url){
+		this.redirect_url = newRedirect_url;
+	}
+	
+	public Date getExpiration_date(){
+		return expiration_date;
+	}
+	
+	public void setExpiration_date(Date newExpiration_date){
+		this.expiration_date = newExpiration_date;
+	}
+	
+	public String getClosed_message(){
+		return closed_message;
+	}
+	
+	public void setClosed_message(String newClosed_message){
+		this.closed_message = newClosed_message;
+	}
 }
--- src/main/java/dash/pojo/Form.java
+++ src/main/java/dash/pojo/Form.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -33,6 +34,8 @@ import dash.security.IAclObject;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Form implements  IAclObject {
 
+	public static enum THEME {PLAIN};
+	
 	/** id of the form */
 	@XmlElement(name = "id")
 	private Long id;
@@ -51,7 +54,42 @@ public class Form implements  IAclObject {
 	@XmlElement(name= "questions")
 	private Set<Question> questions= new HashSet<Question>();
 
+	@XmlElement(name = "enabled")
+	private boolean enabled;
+	
+	@XmlElement(name = "public")
+	private boolean publi;
+	
+	@XmlElement(name = "theme")
+	private THEME theme;
+	
+	@XmlElement(name = "redirect_to_url")
+	private boolean redirect_to_url;
+	
+	@XmlElement(name = "alert_for_response")
+	private boolean alert_for_response;
 
+	@XmlElement(name = "email_embedded_resposes")
+	private boolean email_embedded_responses;
+	
+	@XmlElement(name = "send_confirmation_email")
+	private boolean send_confirmation_email; 
+	
+	@XmlElement(name = "email_message")
+	private String email_message;
+	
+	@XmlElement(name = "completed_message")
+	private String completed_message;
+	
+	@XmlElement(name = "redirect_url")
+	private String redirect_url;
+	
+	@XmlElement(name = "expiration_date")
+	private Date expiration_date;
+	
+	@XmlElement(name = "closed_message")
+	private String closed_message;
+	
 	public Form(FormEntity formEntity) {
 		try {
 			BeanUtils.copyProperties(this, formEntity);
@@ -63,11 +101,27 @@ public class Form implements  IAclObject {
 			e.printStackTrace();
 		}
 	}
-
-	public Form(String name , Set<Question> questions) {
+	
+	public Form(String name , Set<Question> questions, boolean redirect_to_url,
+			boolean enabled, boolean publi, boolean alert_for_response,
+			boolean email_embedded_responses, boolean send_confirmation_email, 
+			String email_message, String completed_message, String redirect_url, 
+			Date expiration_date, String closed_message, THEME theme) {
 		super();
 		this.name = name;
 		this.questions = questions;
+		this.redirect_to_url = redirect_to_url;
+		this.enabled = enabled;
+		this.publi = publi;
+		this.alert_for_response = alert_for_response;
+		this.email_embedded_responses = email_embedded_responses;
+		this.send_confirmation_email = send_confirmation_email;
+		this.email_message = email_message;
+		this.completed_message = completed_message;
+		this.redirect_url = redirect_url;
+		this.expiration_date = expiration_date;
+		this.closed_message = closed_message;
+		this.theme = theme;
 	}
 
 	public Form() {
@@ -90,7 +144,7 @@ public class Form implements  IAclObject {
 	public void setName( String name) {
 		this.name = name;
 	}
-
+	
 	public Long getId() {
 		return id;
 	}
@@ -106,7 +160,101 @@ public class Form implements  IAclObject {
 	public void setinsertion_date( Date insertion_date) {
 		this.insertion_date = insertion_date;
 	}
+	
+	public boolean getEnabled(){
+		return enabled;
+	}
+	
+	public void setEnabled(boolean newEnabled){
+		this.enabled = newEnabled;
+	}
+	
+	public boolean getPubli(){
+		return publi;
+	}
+	
+	public void setPubli(boolean newPubli){
+		this.publi = newPubli;
+	}
 
+	public THEME getTheme(){
+		return theme;
+	}
 
-
+	public void setTheme( THEME newTheme){
+		this.theme = newTheme;
+	}
+	
+	public boolean getRedirect_to_url(){
+		return redirect_to_url;
+	}
+	
+	public void setRedirect_to_url(boolean newRedirect_to_url){
+		this.redirect_to_url = newRedirect_to_url;
+	}
+	
+	public boolean getAlert_for_response(){
+		return alert_for_response;
+	}
+	
+	public void setAlert_for_response(boolean newAlert_for_response){
+		this.alert_for_response = newAlert_for_response;
+	}
+	
+	public boolean getEmail_embedded_responses(){
+		return email_embedded_responses;
+	}
+	
+	public void setEmail_embedded_responses(boolean newEmail_embedded_responses){
+		this.email_embedded_responses = newEmail_embedded_responses;
+	}
+	
+	public boolean getSend_confirmation_email(){
+		return send_confirmation_email;
+	}
+	
+	public void setSend_confirmation_email(boolean newSend_confirmation_email){
+		this.send_confirmation_email = newSend_confirmation_email;
+	}
+	
+	public String getEmail_message(){
+		return email_message;
+	}
+	
+	public void setEmail_message(String newEmail_message){
+		this.email_message = newEmail_message;
+	}
+	
+	public String getCompleted_message(){
+		return completed_message;
+	}
+	
+	public void setCompleted_message(String newCompleted_message){
+		this.completed_message = newCompleted_message;
+	}
+	
+	public String getRedirect_url(){
+		return redirect_url;
+	}
+	
+	public void setRedirect_url(String newRedirect_url){
+		this.redirect_url = newRedirect_url;
+	}
+	
+	public Date getExpiration_date(){
+		return expiration_date;
+	}
+	
+	public void setExpiration_date(Date newExpiration_date){
+		this.expiration_date = newExpiration_date;
+	}
+	
+	public String getClosed_message(){
+		return closed_message;
+	}
+	
+	public void setClosed_message(String newClosed_message){
+		this.closed_message = newClosed_message;
+	}
 }
+
